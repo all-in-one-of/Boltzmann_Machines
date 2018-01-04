7fe27f9e222d3616c8ba6f8cd9f8b943dd3011ca
Mathias Oster <34043360+MatOster@users.noreply.github.com>
2018-01-04 20:09:06 +0100

    Update RBM_2ndOrder.py

--- RBM_2ndOrder.py
+++ RBM_2ndOrder.py
@@ -10,43 +10,63 @@ alpha = 0.5
 batchsize = 100
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-trX, trY, teX, teY = mnist.train.images, mnist.train.labels, mnist.test.images,\
-    mnist.test.labels
-
+trX, trY, teX, teY = mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
+#input and output
 X = tf.placeholder("float", [None, 784])
 Y = tf.placeholder("float", [None, 10])
 
-rbm_w = tf.placeholder("float", [784, 1000])
+#weights and biases
+rbm_w = tf.placeholder("float", [784, 500])
 rbm_vb = tf.placeholder("float", [784])
-rbm_hb = tf.placeholder("float", [1000])
+rbm_hb = tf.placeholder("float", [500])
+
+#Gibbs sampling
 h0 = sample_prob(tf.nn.sigmoid(tf.matmul(X, rbm_w) + rbm_hb))
-v1 = sample_prob(tf.nn.sigmoid(
-    tf.matmul(h0, tf.transpose(rbm_w)) + rbm_vb))
+v1 = sample_prob(tf.nn.sigmoid(tf.matmul(h0, tf.transpose(rbm_w)) + rbm_vb))
 h1 = tf.nn.sigmoid(tf.matmul(v1, rbm_w) + rbm_hb)
+
+#updating rules of 
 w_positive_grad = tf.matmul(tf.transpose(X), h0)
 w_negative_grad = tf.matmul(tf.transpose(v1), h1)
-update_w = rbm_w + alpha * \
-    (w_positive_grad - w_negative_grad) / tf.to_float(tf.shape(X)[0])
+update_w = rbm_w + alpha * (w_positive_grad - w_negative_grad) / tf.to_float(tf.shape(X)[0])
 update_vb = rbm_vb + alpha * tf.reduce_mean(X - v1, 0)
 update_hb = rbm_hb + alpha * tf.reduce_mean(h0 - h1, 0)
 
-#RBM - bi partite
-def RBM_2( hid,vis):
-    hidden = len(hid)    #number of hidden units
-    visible = len(vis)  #number of visible
+#claculating error
+#sample 
+h_sample = sample_prob(tf.nn.sigmoid(tf.matmul(X, rbm_w) + rbm_hb))
+v_sample = sample_prob(tf.nn.sigmoid(tf.matmul(h_sample, tf.transpose(rbm_w)) + rbm_vb))
+#error
+err = X - v_sample
+err_sum = tf.reduce_mean(err * err)
 
-    #initialize weights and biases
-    weight =  np.random.rand(visible,hidden)
-    bias_v = np.random.rand(visible,1)
-    bias_h = np.random.rand(visible,1)
+#initializing session
+sess = tf.Session()
+init = tf.global_variables_initializer()
+sess.run(init)
 
-    # define Energy
+n_w = np.zeros([784, 500], np.float32)
+n_vb = np.zeros([784], np.float32)
+n_hb = np.zeros([400], np.float32)
+o_w = np.zeros([784, 500], np.float32)
+o_vb = np.zeros([784], np.float32)
+o_hb = np.zeros([500], np.float32)
+print(sess.run(err_sum, feed_dict={X: trX, rbm_w: o_w, rbm_vb: o_vb, rbm_hb: o_hb}))
 
-    energy = - np.dot(bias_v,vis)-np.dot(bias_h,hid)- np.dot(vis,np.dot(weight,hid))
-
+#run training and use generativity
+for start, end in zip(range(0, len(trX), batchsize), range(batchsize, len(trX), batchsize)):
+    batch = trX[start:end]
+    n_w = sess.run(update_w, feed_dict={
+                   X: batch, rbm_w: o_w, rbm_vb: o_vb, rbm_hb: o_hb})
+    n_vb = sess.run(update_vb, feed_dict={
+                    X: batch, rbm_w: o_w, rbm_vb: o_vb, rbm_hb: o_hb})
+    n_hb = sess.run(update_hb, feed_dict={
+                    X: batch, rbm_w: o_w, rbm_vb: o_vb, rbm_hb: o_hb})
+    o_w = n_w
+    o_vb = n_vb
+    o_hb = n_hb
     if start % 10000 == 0:
-        print(sess.run(
-            err_sum, feed_dict={X: trX, rbm_w: n_w, rbm_vb: n_vb, rbm_hb: n_hb}))
+        print(sess.run(err_sum, feed_dict={X: trX, rbm_w: n_w, rbm_vb: n_vb, rbm_hb: n_hb}))
         image = Image.fromarray(
             tile_raster_images(
                 X=n_w.T,
@@ -55,4 +75,6 @@ def RBM_2( hid,vis):
                 tile_spacing=(1, 1)
             )
         )
-        image.save("rbm_%d.png" % (start / 100000))    
+        image.show()
+
+    
