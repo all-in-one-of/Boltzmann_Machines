aecd4c5bfed04abaf3f4dbf8e3aa996b7f6f5181
Mathias Oster <34043360+MatOster@users.noreply.github.com>
2018-01-04 15:36:45 +0100

    Update RBM_2ndOrder.py

--- RBM_2ndOrder.py
+++ RBM_2ndOrder.py
@@ -6,7 +6,29 @@ import input_data
 from PIL import Image
 from Util import tile_raster_images
 
+alpha = 0.5
+batchsize = 100
 
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+trX, trY, teX, teY = mnist.train.images, mnist.train.labels, mnist.test.images,\
+    mnist.test.labels
+
+X = tf.placeholder("float", [None, 784])
+Y = tf.placeholder("float", [None, 10])
+
+rbm_w = tf.placeholder("float", [784, 1000])
+rbm_vb = tf.placeholder("float", [784])
+rbm_hb = tf.placeholder("float", [1000])
+h0 = sample_prob(tf.nn.sigmoid(tf.matmul(X, rbm_w) + rbm_hb))
+v1 = sample_prob(tf.nn.sigmoid(
+    tf.matmul(h0, tf.transpose(rbm_w)) + rbm_vb))
+h1 = tf.nn.sigmoid(tf.matmul(v1, rbm_w) + rbm_hb)
+w_positive_grad = tf.matmul(tf.transpose(X), h0)
+w_negative_grad = tf.matmul(tf.transpose(v1), h1)
+update_w = rbm_w + alpha * \
+    (w_positive_grad - w_negative_grad) / tf.to_float(tf.shape(X)[0])
+update_vb = rbm_vb + alpha * tf.reduce_mean(X - v1, 0)
+update_hb = rbm_hb + alpha * tf.reduce_mean(h0 - h1, 0)
 
 #RBM - bi partite
 def RBM_2( hid,vis):
